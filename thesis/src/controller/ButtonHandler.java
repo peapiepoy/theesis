869f4091be641de0107d31a66a0d941352ca9030
Shara Molina <sharamor88@gmail.com>
2018-05-02 16:53:18 +0800

    ghj

--- thesis/src/controller/ButtonHandler.java
+++ thesis/src/controller/ButtonHandler.java
@@ -23,6 +23,7 @@ public class ButtonHandler implements ActionListener{
 		TargetSelection.getInstance().reset.addActionListener(this);
 		TargetSelection.getInstance().submit.addActionListener(this);
 		SegmentationPanel.getInstance().segment.addActionListener(this);
+		System.out.println("hhhhh");
 	}
 
 	@Override
