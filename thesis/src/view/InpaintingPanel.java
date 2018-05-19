f6224d946154c70454c1731ae339e588f3a5183f
computer <computer@computer-H110M-C3D-C3V>
2018-05-20 05:42:01 +0800

    creation of IP 2 lines huhu

---
+++ thesis/src/view/InpaintingPanel.java
@@ -0,0 +1,15 @@
+package view;
+
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+public class InpaintingPanel extends JPanel{
+	public static InpaintingPanel instance;
+	public JButton inpaint;
+	
+	
+	public InpaintingPanel() {
+		
+	}
+
+}
