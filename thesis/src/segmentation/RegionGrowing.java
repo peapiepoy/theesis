71625ccaa99e007f050894b373a396bdd809d014
hannahcorcilles <hacorcilles@up.edu.ph>
2018-10-04 11:00:16 +0800

    laptop version

---
+++ thesis/src/segmentation/RegionGrowing.java
@@ -0,0 +1,27 @@
+package segmentation;
+
+import java.awt.image.BufferedImage;
+
+import main.Entry;
+
+public class RegionGrowing {
+	public BufferedImage img, grayscaled, segmentedImage;
+	private int width, height;
+	
+	public RegionGrowing(BufferedImage img) {
+		this.img = img;
+		this.width = img.getWidth();
+		this.height = img.getHeight();
+		grayscaled = new BufferedImage(width, height, 
+			      BufferedImage.TYPE_BYTE_GRAY);
+		
+		grayscaled = Entry.getInstance().grayscaling();
+	}
+	
+	public void setImage(BufferedImage displayImage) {
+		this.segmentedImage = displayImage;
+	}
+	public BufferedImage getSegmentedImage() {
+		return grayscaled;
+	}
+}
